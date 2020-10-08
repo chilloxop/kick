ae755999613d16c408941b6d02054f0bfafa53e6
chilloxop <71260756+chilloxop@users.noreply.github.com>
2020-10-08 15:38:24 +1030

    Web scrapper V1.0
    Just a simple scrapper I was bored lolmao.

---
+++ kck_scrap--v.1.py
@@ -0,0 +1,47 @@
+__author__ = 'JonesBBQ'
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = input('\nEnter URL you want to scrap : ')  # makes it more user friendly i guess (note: its full address)
+site_data = requests.get(url)
+html_element = input('\nEnter elements to target with BeautifulSoup  ||e.g body, table, iframe||  : ')
+special_class_tag = input('Enter special class tag ||leave blank for any||  : ')
+
+
+class sc: # class for all items you want idk
+    phones = re.findall(r'(\(?[0-9]{3}\)?(?:\-|\s|\.)?[0-9]{3}(?:\-|\.)[0-9]{4})', site_data.text)
+    emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', site_data.text)
+
+
+soup = BeautifulSoup(site_data.text, 'html.parser')
+
+special_html_tag = soup.find(html_element, {'class': special_class_tag})
+
+print('\n######################  ||  REGEX PLAIN PHONE AND EMAIL ||  ######################')
+print('\n', sc.phones, sc.emails)
+print('\n######################  ||  SOUP PARSED  ||  ######################')
+print('\n', soup.text)
+print('\n######################  ||  RAW SOUP HTML  ||  ######################')
+print('\n', special_html_tag)
+print('\n\n\n\n\n\n\n\n\n\n')
+print('\nEnd of task')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
