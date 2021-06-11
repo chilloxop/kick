345bcea050711faa16d36f80faf7be16ec50d564
chilloxop <71260756+chilloxop@users.noreply.github.com>
2021-06-11 16:31:39 +0930

    ignore just a trojan for KCK-OS
    https://replit.com/@chilloxop/KCK-OS2

---
+++ static.py
@@ -0,0 +1,16 @@
+import os 
+import time
+while True:	
+	os.system("bash -c 'cd scripts && python img-viewer.py noise.png'")
+	time.sleep(1)
+os.system("bash -c 'mkdir static'")
+os.system("bash -c 'mkdir error'")
+os.system("bash -c 'mkdir when'")
+os.system("bash -c 'mkdir air'")
+os.system("bash -c 'mkdir number'")
+os.system("bash -c 'mkdir who'")
+os.system("bash -c 'mkdir wins'")
+os.system("bash -c 'mkdir parasite'")
+os.system("bash -c 'mkdir cypher'")
+os.system("bash -c 'mkdir maybe'")
+os.system("bash -c 'mkdir dead'")
